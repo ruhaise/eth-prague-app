2c758fe327d4f0ee9f7ef6df2d2c9a8751b1cf8e
Joaquin <joaquin.garcia.nieto@accenture.com>
2022-06-10 19:20:09 +0200

    feat:added styling

--- src/pages/Segment.jsx
+++ src/pages/Segment.jsx
@@ -1,8 +1,46 @@
 import React, { useState } from "react";
 import { Button, TextField } from "@material-ui/core";
+import styled from "styled-components";
+import { useDispatch } from "react-redux";
+import { saveSegment } from "../redux/segment/actions";
 
+const StyledWrapper = styled.div`
+  text-align: center;
+  padding: 24px;
+`;
+
+const StyledInputsWrapper = styled.div`
+  width: 90%;
+  margin: auto;
+  max-width: 500px;
+  display: flex;
+  margin-bottom: 15px;
+`;
+const StyledInputsRemove = styled.div`
+  width: 40px;
+  align-self: flex-end;
+  margin-left: 10px;
+`;
+
+const StyledButton = styled(Button)`
+  width: 200px;
+`;
+
+const StyledInputsoutWrapp = styled.div`
+  margin-bottom: 30px;
+`;
+
+const StyledButtonsWrapper = styled.div`
+  width: 90%;
+  margin: auto;
+  max-width: 500px;
+  display: flex;
+  justify-content: space-around;
+`;
 const Segment = () => {
   const [segmentQuery, setSegmentQuery] = useState([{ contract_address: "" }]);
+
+  const dispatch = useDispatch();
 
   const handleAddSegment = () => {
     setSegmentQuery([...segmentQuery, { contract_address: "" }]);
@@ -21,41 +59,58 @@ const Segment = () => {
     setSegmentQuery(segmentList);
   };
 
-  return (
-    <>
-      {segmentQuery.map((segment, index) => {
-        return (
-          <div>
-            {" "}
-            <TextField
-              required
-              label="contract_address"
-              name="contract_address"
-              defaultValue="contract address"
-              value={segment.contract_address}
-              onChange={(e) => handleSegmentChange(index, e)}
-            />
-            {index > 0 && (
-              <Button
-                onClick={() => handleRemoveSegment(index)}
-                color="primary"
-              >
-                Remove
-              </Button>
-            )}
-          </div>
-        );
-      })}
+  const handleSubmit = () => {
+    dispatch(saveSegment(segmentQuery));
+  };
 
-      <Button
-        onClick={handleAddSegment}
-        disabled={segmentQuery.length > 5}
-        color="primary"
-      >
-        {" "}
-        Add +
-      </Button>
-    </>
+  return (
+    <StyledWrapper>
+      <StyledInputsoutWrapp>
+        {segmentQuery.map((segment, index) => {
+          return (
+            <StyledInputsWrapper key={index}>
+              {" "}
+              <TextField
+                required
+                label="Contract address"
+                name="contract_address"
+                defaultValue="contract address"
+                value={segment.contract_address}
+                onChange={(e) => handleSegmentChange(index, e)}
+                fullWidth
+              />
+              <StyledInputsRemove>
+                {index > 0 && (
+                  <Button
+                    variant="outlined"
+                    size="medium"
+                    onClick={() => handleRemoveSegment(index)}
+                    color="primary"
+                  >
+                    Remove
+                  </Button>
+                )}
+              </StyledInputsRemove>
+            </StyledInputsWrapper>
+          );
+        })}
+      </StyledInputsoutWrapp>
+      <StyledButtonsWrapper>
+        <StyledButton
+          onClick={handleAddSegment}
+          disabled={segmentQuery.length > 4}
+          color="primary"
+          variant="contained"
+          size="large"
+        >
+          Add +
+        </StyledButton>
+
+        <StyledButton onClick={handleSubmit} size="large" variant="contained">
+          SEND
+        </StyledButton>
+      </StyledButtonsWrapper>
+    </StyledWrapper>
   );
 };
 
