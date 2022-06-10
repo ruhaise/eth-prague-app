ac45f23fd583e8892bcb4c85bebfb5dc1c9be998
oyershov <oleksandr.yershov@sinnerschrader.com>
2022-06-10 19:25:50 +0200

    add gql request

--- src/pages/GQL.jsx
+++ src/pages/GQL.jsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const GQL = () => {
   const fetchGQL = async () => {
-    const payload = `\ttransfers(\n    where: {collection: \"0xdb3b2e1f699caf230ee75bfbe7d97d70f81bc945\"},\n    first: 10,\n    orderBy: timestamp,\n    orderDirection: desc\n  ) {\n    amount\n    timestamp\n    token {identifier}\n    senderAddress {id}\n    receiverAddress {id}\n    collection {name,id}\n    matchedSale {platform, currency {symbol}}\n  }"`;
+    const payload = `{\n  tokens(where: {\n    collection: \"0xbc4ca0eda7647a8ab7c2061c2e118a18a936f13d\",\n  }) {\n    owner {\n      id\n    }\n  }\n}`;
     const data = await axios.post(
-      `https://api.thegraph.com/subgraphs/name/dontbuyuncle/matic-silver-pool`,
+      `https://gateway.thegraph.com/api/2d3d83f6c8345633d0dce29d41068a6b/subgraphs/id/B333F7Ra4kuVBSwHFDfH9x9N1341GYHvdfpV94KY8Gmv`,
       { query: payload }
     );
     
